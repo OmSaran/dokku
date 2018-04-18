dcc92017864dcdfb77c8c0b2bd39436e4b99c015
om <omiyorulz@gmail.com>
2018-04-18 22:43:49 +0530

    jsdnflfsd

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var express = require('express');
 var PORT = process.env.PORT
 var app = express()
 
+app.get('/', function(req, res) {
+    res.send('Hello world!');
+})
+
 
 var listener = app.listen(PORT || 3000, function() { console.log(listener.address().port); })
